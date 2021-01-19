796a651367270132efa2b10f7216e263d81926c0
Youssef <youssef.najeh@smals.be>
2021-01-19 02:36:26 +0100

    add tomcat dependency and  servlet initializer + controller

--- src/main/java/be/najeh/devopstest/DevopsTestApplication.java
+++ src/main/java/be/najeh/devopstest/DevopsTestApplication.java
@@ -2,12 +2,17 @@ package be.najeh.devopstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DevopsTestApplication {
+public class DevopsTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DevopsTestApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DevopsTestApplication.class);
+    }
 }
---
+++ src/main/java/be/najeh/devopstest/web/HomeController.java
@@ -0,0 +1,13 @@
+package be.najeh.devopstest.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "test";
+    }
+}
