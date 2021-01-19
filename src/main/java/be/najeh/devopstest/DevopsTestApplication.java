8daf178d2ffeee1999fe053e1b8de0f48cdc8964
Youssef <youssef.najeh@smals.be>
2021-01-19 02:25:17 +0100

    creation of the spring project

---
+++ src/main/java/be/najeh/devopstest/DevopsTestApplication.java
@@ -0,0 +1,13 @@
+package be.najeh.devopstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/najeh/devopstest/DevopsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package be.najeh.devopstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
